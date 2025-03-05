ef20da26ddfc13b5085699d4b38561e5c3221a26
semenkodenis <semenkodanis@gmail.com>
2025-03-05 10:37:13 +0200

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+class Program
+{
+    static void Main()
+    {
+        List<Dictionary<string, int>> input = new List<Dictionary<string, int>>
+        {
+            new Dictionary<string, int> { { "item1", 400 } },
+            new Dictionary<string, int> { { "item2", 300 } },
+            new Dictionary<string, int> { { "item1", 750 } }
+        };
+
+        Dictionary<string, int> result = new Dictionary<string, int>();
+
+        foreach (var dict in input)
+        {
+            foreach (var pair in dict)
+            {
+                string key = pair.Key;
+                int value = pair.Value;
+
+                if (result.ContainsKey(key))
+                {
+                    result[key] += value;
+                }
+                else
+                {
+                    result[key] = value;
+                }
+            }
+        }
+
+        string jsonResult = JsonSerializer.Serialize(result, new JsonSerializerOptions { WriteIndented = true });
+        
+        System.IO.File.WriteAllText("result.json", jsonResult);
+
+        Console.WriteLine(jsonResult);
+    }
+}
